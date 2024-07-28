01523f93bacf5f2770dfd8e54d8971295d66e550
Nilanjan Debnath <91799113+nilanjan-debnath@users.noreply.github.com>
2024-07-28 22:35:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
-pip install -r requirments.txt
-python3.9 manage.py collectstatic
+echo "BUILD START"
+
+python3.9 -m venv venv
+source venv/bin/activate
+python.exe -m pip install --upgrade pip
+pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
+
+echo "BUILD END"
